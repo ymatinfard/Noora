977197c7cb18bf6d7e26da65384f33cdf7c00f32
Yousef <matinfard.y@gmail.com>
2025-07-13 18:06:11 +0200

    Add Flow.asResult() extension function to handle flow state

--- app/src/main/java/com/matin/noora/core/common/Result.kt
+++ app/src/main/java/com/matin/noora/core/common/Result.kt
@@ -1,7 +1,20 @@
 package com.matin.noora.core.common
 
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.catch
+import kotlinx.coroutines.flow.map
+import kotlinx.coroutines.flow.onStart
+
 sealed interface Result<out T> {
     data class Success<out T>(val data: T) : Result<T>
     data class Error(val exception: Exception) : Result<Nothing>
     object Loading : Result<Nothing>
+}
+
+fun <T> Flow<T>.asResult(): Flow<Result<T>> = this.map { data ->
+    Result.Success(data)
+}.catch { exception ->
+    Result.Error(exception as Exception)
+}.onStart {
+    Result.Loading
 }
