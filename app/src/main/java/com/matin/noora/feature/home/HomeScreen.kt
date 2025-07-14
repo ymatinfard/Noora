00655b88ede33660f96e029f45078cc276053718
Yousef <matinfard.y@gmail.com>
2025-07-14 19:41:45 +0200

    Add HomeTopBar

---
+++ app/src/main/java/com/matin/noora/feature/home/HomeScreen.kt
@@ -0,0 +1,112 @@
+package com.matin.noora.feature.home
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.matin.noora.R
+import com.matin.noora.designsystem.NooraTheme
+
+@Composable
+fun HomeScreenRoute() {
+
+    HomeScreen()
+}
+
+@Composable
+fun HomeScreen() {
+    Column {
+        HomeTopBar("USF")
+    }
+}
+
+@Composable
+fun HomeTopBar(name: String, onProfileClicked: () -> Unit = {}) {
+
+    Row(
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(horizontal = 16.dp),
+        verticalAlignment = Alignment.CenterVertically,
+    ) {
+        ProfileName(name)
+        Spacer(modifier = Modifier.weight(1f))
+        Row(
+            verticalAlignment = Alignment.CenterVertically,
+            horizontalArrangement = Arrangement.spacedBy(16.dp),
+        ) {
+            InviteFriendIcon()
+            ProfileIcon(name)
+        }
+    }
+}
+
+@Composable
+private fun ProfileName(name: String) {
+    Text("Hi, $name!", style = MaterialTheme.typography.headlineMedium)
+}
+
+@Composable
+private fun InviteFriendIcon() {
+    Box(
+        modifier = Modifier
+            .size(48.dp)
+            .clip(shape = CircleShape)
+            .background(MaterialTheme.colorScheme.surface),
+        contentAlignment = Alignment.Center,
+    ) {
+        Icon(
+            painter = painterResource(R.drawable.ic_gift),
+            contentDescription = "Profile Icon",
+            tint = MaterialTheme.colorScheme.onSurface,
+        )
+    }
+}
+
+@Composable
+fun ProfileIcon(name: String) {
+    Box(
+        modifier = Modifier
+            .size(64.dp)
+            .clip(shape = CircleShape)
+            .background(MaterialTheme.colorScheme.primary)
+            .border(
+                width = 2.dp,
+                color = MaterialTheme.colorScheme.outline,
+                shape = CircleShape
+            ),
+        contentAlignment = Alignment.Center,
+
+        ) {
+        Text(
+            text = name.first().toString(), color = MaterialTheme.colorScheme.onPrimary,
+            style = MaterialTheme.typography.displayMedium
+        )
+    }
+}
+
+
+@Preview(showBackground = true)
+@Composable
+private fun HomeTopBarPreview() {
+    NooraTheme {
+        HomeTopBar(name = "Ali", onProfileClicked = {})
+    }
+}
