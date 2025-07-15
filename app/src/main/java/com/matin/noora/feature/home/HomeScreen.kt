99cbb872193a0b8e339829762bafd030995565b7
Yousef <matinfard.y@gmail.com>
2025-07-15 10:20:42 +0200

    Add home HelpCard

--- app/src/main/java/com/matin/noora/feature/home/HomeScreen.kt
+++ app/src/main/java/com/matin/noora/feature/home/HomeScreen.kt
@@ -1,5 +1,6 @@
 package com.matin.noora.feature.home
 
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Arrangement
@@ -11,6 +12,11 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.automirrored.filled.Send
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
@@ -18,7 +24,9 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
+import androidx.compose.ui.draw.rotate
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.matin.noora.R
@@ -34,6 +42,79 @@ fun HomeScreenRoute() {
 fun HomeScreen() {
     Column {
         HomeTopBar("USF")
+        HelpCard()
+    }
+}
+
+@Composable
+fun HelpCard() {
+    Card(
+        modifier = Modifier
+            .fillMaxWidth(),
+        shape = RoundedCornerShape(8.dp),
+        elevation = CardDefaults.cardElevation(2.dp)
+    ) {
+        Column(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(horizontal = 16.dp, vertical = 8.dp),
+        ) {
+            Row(
+                verticalAlignment = Alignment.CenterVertically
+            ) {
+                Image(
+                    painter = painterResource(id = R.drawable.noora),
+                    contentDescription = "Help Icon",
+                    modifier = Modifier
+                        .size(64.dp)
+                        .clip(CircleShape)
+                        .background(MaterialTheme.colorScheme.primary)
+                        .border(
+                            width = 2.dp,
+                            color = MaterialTheme.colorScheme.outline,
+                            shape = CircleShape
+                        )
+                )
+                Text(
+                    modifier = Modifier.padding(start = 8.dp),
+                    text = stringResource(R.string.what_can_i_help_you_with),
+                    style = MaterialTheme.typography.titleLarge,
+                )
+            }
+            Spacer(modifier = Modifier.padding(vertical = 4.dp))
+            Row(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .clip(RoundedCornerShape(8.dp))
+                    .border(
+                        width = 1.dp,
+                        color = MaterialTheme.colorScheme.outline,
+                        shape = RoundedCornerShape(8.dp)
+                    )
+                    .padding(8.dp),
+                verticalAlignment = Alignment.CenterVertically
+            ) {
+                Text(stringResource(R.string.message), color = MaterialTheme.colorScheme.outlineVariant)
+                Spacer(modifier = Modifier.weight(1f))
+                Box(
+                    modifier = Modifier
+                        .clip(CircleShape)
+                        .background(MaterialTheme.colorScheme.primary),
+                    contentAlignment = Alignment.Center,
+
+                    ) {
+                    Icon(
+                        modifier = Modifier
+                            .size(24.dp)
+                            .padding(6.dp)
+                            .rotate(-45f),
+                        tint = MaterialTheme.colorScheme.onPrimary,
+                        imageVector = Icons.AutoMirrored.Filled.Send,
+                        contentDescription = "Send Icon",
+                    )
+                }
+            }
+        }
     }
 }
 
@@ -107,6 +188,7 @@ fun ProfileIcon(name: String) {
 @Composable
 private fun HomeTopBarPreview() {
     NooraTheme {
-        HomeTopBar(name = "Ali", onProfileClicked = {})
+        // HomeTopBar(name = "Ali", onProfileClicked = {})
+        HelpCard()
     }
 }
