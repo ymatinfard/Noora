e462d96af77adccbd945a39440b52a1a210a87d2
Yousef <matinfard.y@gmail.com>
2025-07-25 17:48:03 +0200

    Add NooraIcons

---
+++ app/src/main/java/com/matin/noora/designsystem/NooraIcons.kt
@@ -0,0 +1,12 @@
+package com.matin.noora.designsystem
+
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.automirrored.filled.ArrowBack
+import androidx.compose.material.icons.filled.Info
+import androidx.compose.material.icons.filled.Search
+
+object NooraIcons {
+    val INFO = Icons.Default.Info
+    val SEARCH = Icons.Default.Search
+    val ARROW_BACK = Icons.AutoMirrored.Filled.ArrowBack
+}
