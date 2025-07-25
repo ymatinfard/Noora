38a650e0cfe4222d3f2e75a1d0c3eaace7690abc
Yousef <matinfard.y@gmail.com>
2025-07-25 17:44:54 +0200

    Add MessageTimeStamp

---
+++ app/src/main/java/com/matin/noora/designsystem/component/MessageTimeStamp.kt
@@ -0,0 +1,23 @@
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.sp
+import com.matin.noora.designsystem.component.TIMESTAMP_TEXT_SIZE
+import com.matin.noora.designsystem.component.chooseOnSurfaceColorFor
+import java.text.SimpleDateFormat
+
+@Composable
+fun MessageTimeStamp(
+    timeStamp: Long,
+    isFromCurrentUser: Boolean,
+    modifier: Modifier = Modifier
+) {
+    Text(
+        text = formatTimestamp(timeStamp),
+        fontSize = TIMESTAMP_TEXT_SIZE.sp,
+        color = chooseOnSurfaceColorFor(isFromCurrentUser),
+        modifier = modifier
+    )
+}
+
+fun formatTimestamp(timeStamp: Long): String = SimpleDateFormat("HH:mm a").format(timeStamp)
